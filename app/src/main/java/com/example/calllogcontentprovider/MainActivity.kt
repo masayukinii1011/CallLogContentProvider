d46ab9ce503bafd974237b43d08516782e54eb8b
masayukinii1011 <masayukinii1011@gmail.com>
2020-05-04 12:52:39 +0900

    add comment

--- app/src/main/java/com/example/calllogcontentprovider/MainActivity.kt
+++ app/src/main/java/com/example/calllogcontentprovider/MainActivity.kt
@@ -12,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //contentResolver.queryで抽出したい項目
     var cols = listOf<String>(
         CallLog.Calls._ID,
         CallLog.Calls.NUMBER,
@@ -23,6 +24,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /**
+         * パーミッションの許可が無い場合、許可ダイアログを表示
+         */
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.READ_CALL_LOG
@@ -38,6 +42,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * 許可ダイアログの結果を受け取る
+     */
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -51,23 +58,27 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("MissingPermission")
     private fun displayLog() {
+        //SQLiteのCursor
+        //(検索したいURI, 抽出したい項目, 絞り込み条件, 絞り込みパラメータ, ソート)
+        val rs = contentResolver.query(
+            CallLog.Calls.CONTENT_URI,
+            cols, null, null, "${CallLog.Calls.LAST_MODIFIED} DESC"
+        )
 
-        var from = listOf<String>(
+        //表示するColumn
+        val from = listOf<String>(
             CallLog.Calls.NUMBER,
             CallLog.Calls.DURATION,
             CallLog.Calls.TYPE
         ).toTypedArray()
 
-        var to = intArrayOf(R.id.textView1, R.id.textView2, R.id.textView3)
+        //バインドするID
+        val to = intArrayOf(R.id.textView1, R.id.textView2, R.id.textView3)
 
-        var rs = contentResolver.query(
-            CallLog.Calls.CONTENT_URI,
-            cols, null, null, "${CallLog.Calls.LAST_MODIFIED} DESC"
-        )
-
-        var adapter =
+        //ListViewへ渡す
+        //(コンテキスト,表示するレイアウト,SQLiteのCursor,表示するColumn,バインドするID,？)
+        val adapter =
             SimpleCursorAdapter(applicationContext, R.layout.mylayout, rs, from, to, 0)
-
         listview.adapter = adapter
     }
 }
